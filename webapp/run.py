47168204fd29c11882f96c0efa0d3a9d7fdf6935
Mukesh Regar <mukeshr@amdocs.com>
2020-12-22 15:21:39 +0530

    Removing redundent prints.

--- webapp/run.py
+++ webapp/run.py
@@ -1,14 +1,12 @@
+import joblib
 import json
-import plotly
 import pandas as pd
-
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-
+import plotly
 from flask import Flask
 from flask import render_template, request
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
 from plotly.graph_objs import Bar, Pie
-import joblib
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
@@ -34,7 +32,6 @@ df = pd.read_sql_table('disaster_messages', engine)
 model = joblib.load("../models/classifier.pkl")
 
 
-
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
@@ -48,9 +45,7 @@ def index():
     category_df = df.drop(columns=['message', 'id', 'original', 'genre'])
     col_sum = category_df.sum()
     column_names = list(col_sum.index)
-    print(column_names)
     column_counts = col_sum.values
-    print(column_counts)
     graphs = [
         {
             'data': [
